1f65eebd58d9eead17b2f6676c6d41061fda2983
Ebtehal Alfaifi <ebtehal.alfaifi@outlook.sa>
2024-10-28 20:45:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+//        1 - Write a Java method to find the smallest number among three
+//        numbers.
+        Scanner eb = new Scanner(System.in);
+//        System.out.println(" enter number 1 ");
+//        int num1 = eb.nextInt();
+//        System.out.println(" enter number 2 "); ///////////////////// استدعاء ميثود اصغر رقم
+//        int num2 = eb.nextInt();
+//        System.out.println(" enter number to 3 ");
+//        int num3 = eb.nextInt();
+////       smallnum(num1,num2,num3);
+
+//
+//   public static void smallnum(int num1, int num2, int num3){
+//        if (num1<=num2&&num1<=num3){
+//            int smallest=0;
+//            num1+=smallest;
+//            System.out.println("the smallest number is " + " "+ num1);
+//
+//        } else if (num2<=num3) {
+//            int smallest=0;
+//            num2+=smallest;
+//            System.out.println(num2);
+//
+//        }else System.out.println("smallest number is " +" "+num3);
+//       return ;
+//        }}
+
+//********************************************************************************************
+
+//2 - Write a Java method that check if the entered number is negative or
+//    positive or zero.
+
+//        System.out.println(" enter number ");
+//int num= eb.nextInt();
+//checknum(num);                                        checknum  استدعاء ميثود
+//
+//    }
+
+
+//    public static void checknum(int n){
+//        if (n>0){
+//            System.out.println(n+""+ " is postive number");
+//        } else if (n<0) {
+//            System.out.println(n +  " " + "is negetive numper");
+//
+//        }else {
+//            System.out.println(" is zero  ");}
+//return;
+//    }}
+
+//****************************************************************************************
+
+
+//        3 - Write a Java method to check whether a string is a valid password.
+//        Password rules:
+
+
+//        System.out.print("Enter a password: ");
+//        String password = eb.nextLine();
+//
+//        if (pass(password)) {                         استدعيت دالة password
+//            System.out.println("Password is valid: " + password);
+//        } else {
+//            System.out.println("Password is invalid: " + password);
+//        }}
+
+//        public static boolean pass(String password) {
+//            if (password.length() < 8) {
+//                return false;
+//            }
+//            int digitCount = 0;
+//            for (int i = 0; i < password.length(); i++) {
+//                char ch = password.charAt(i);
+//                if (!Character.isLetterOrDigit(ch)) {
+//                    return false;
+//                }
+//                if (Character.isDigit(ch)) {
+//                    digitCount++;
+//                }}
+//            return digitCount >= 2;
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
